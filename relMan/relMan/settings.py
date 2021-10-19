5fc23fb9693b2c44ff2430e178c9137680798f04
raja-chaudhary <raja@rajachaudhary.com>
2021-10-19 00:18:20 -0700

    Moved AllAuth templates to BASE_DIR

--- relMan/relMan/settings.py
+++ relMan/relMan/settings.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = 'relMan.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR, 'templates'],
+        'DIRS': [BASE_DIR, 'templates', 'allauth'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
