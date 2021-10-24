6a3b4383fad5aaa7cd8b3dfb7461cd936d58a3f5
raja-chaudhary <raja@rajachaudhary.com>
2021-10-24 00:58:04 -0700

    Added styling to Promises app

--- relMan/promises/forms.py
+++ relMan/promises/forms.py
@@ -17,15 +17,15 @@ class PromiseForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         self.fields['title'].widget.attrs.update(
             {
-                'class': 'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-md',
+                'class': 'border rounded w-full py-2 px-4 text-purple-500 shadow-sm leading-none focus:outline-none focus:ring focus:ring-purple-600 focus:ring-opacity-50 placeholder-purple-500 bg-purple-50',
                 'placeholder': 'Add new promise...'})
         self.fields['content'].widget.attrs.update(
             {
-                'class': 'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline text-md',
+                'class': 'border rounded w-full py-2 px-4 text-purple-500 shadow-sm leading-none focus:outline-none focus:ring focus:ring-purple-600 focus:ring-opacity-50 placeholder-purple-500 bg-purple-50',
                 'placeholder': 'Add promise context...'
             })
         self.fields['deliver_by'].widget.attrs.update(
             {
-                'class': 'w-full pl-4 pr-10 py-3 leading-none rounded-lg shadow-sm focus:outline-none text-gray-600 font-medium focus:ring focus:ring-blue-600 focus:ring-opacity-50',
+                'class': 'border border-purple-600 rounded w-full pl-4 pr-10 py-3 text-purple-500 shadow-sm leading-none focus:outline-none focus:ring focus:ring-purple-600 focus:ring-opacity-50 placeholder-purple-500 bg-purple-50',
                 'placeholder': 'Select date...'
             })
