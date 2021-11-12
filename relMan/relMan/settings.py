92df7fc05788db8e21dfcf2654c233cccb591b6e
raja-chaudhary <raja@rajachaudhary.com>
2021-11-12 11:51:14 -0800

    Updated the allowed hosts

--- relMan/relMan/settings.py
+++ relMan/relMan/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = ['www.dudap.xyz', '127.0.0.1']
+ALLOWED_HOSTS = ['dudap.herokuapp.com', 'www.dudap.xyz', '127.0.0.1']
 
 
 # Application definition
