d5235a580a3b570f9fe870fa0ff55e2bfcf3b8bd
raja-chaudhary <raja@rajachaudhary.com>
2021-11-12 01:45:06 -0800

    Updated SECRET_KEY in settings.py

--- relMan/relMan/settings.py
+++ relMan/relMan/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6s^&ggv(%-r1ql+q=omojki@0*=^=q49nx(g(ip$4q-4--w%*j'
+# SECRET_KEY = 'django-insecure-6s^&ggv(%-r1ql+q=omojki@0*=^=q49nx(g(ip$4q-4--w%*j'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dudap.herokuapp.com']
 
 
 # Application definition
